fc2b401a98b2dec793ca4cb149d8dfecaab236b9
in1011558-byte <in19820822@gmail.com>
2025-10-03 00:34:10 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 # models/__init__.py
-from extensions import db
+from .extensions import db
 from .user import User
 from .school import School
-from .textbook import Book
+from .textbook import Textbook # ここをBookからTextbookに修正
 from .order import Order, OrderItem
+from .cart import Cart
